0a78aee23f504277feb336cdfe64c0f9ad54464c
hateganfloringeorge <hateganfloringeorge@gmail.com>
2022-12-16 15:08:02 +0200

    Separated logic in functions

--- 1/first.py
+++ 1/first.py
@@ -1,26 +1,34 @@
 # Greedy comparison
 import sys
 
+def part_one():
+    biggest_num = 0
+    partial_sum = 0
+
+    #just iterate and compare biggest_num with partial sum when newline
+    for i in range(len(calories)):
+
+        if (calories[i] == '\n'):
+            if (partial_sum > biggest_num):
+                biggest_num = partial_sum
+            partial_sum = 0
+
+        else:
+            value = int(calories[i])
+            partial_sum += value
+
+    # compare with the latest partial sum
+    if (partial_sum > biggest_num):
+        biggest_num = partial_sum
+
+    print(biggest_num)
+
+
+def part_two():
+    print("TODO")
+
 with open('./1/input.txt') as f:
         calories = f.readlines()
 
-biggest_num = 0
-partial_sum = 0
-
-#just iterate and compare biggest_num with partial sum when newline
-for i in range(len(calories)):
-
-    if (calories[i] == '\n'):
-        if (partial_sum > biggest_num):
-            biggest_num = partial_sum
-        partial_sum = 0
-
-    else:
-        value = int(calories[i])
-        partial_sum += value
-
-# compare with the latest partial sum
-if (partial_sum > biggest_num):
-    biggest_num = partial_sum
-
-print(biggest_num)
+#part_one()
+part_two()
