b3ef15917a5fa9e9ce35003758c8ef4968da91db
hateganfloringeorge <hateganfloringeorge@gmail.com>
2022-12-16 19:55:46 +0200

    Second day part two

--- 2/second.py
+++ 2/second.py
@@ -20,7 +20,23 @@ def result_points(opponent, player):
         return 6
     else:
         return 0
+
+def compute_shape(opponent, result):
+    if (result == 1):
+        return opponent
+
+    if (result == 0 and opponent == 0):
+        return 2
     
+    if (result == 2 and opponent == 2):
+        return 0
+
+    if (result == 0):
+        return opponent - 1
+    else:
+        return opponent + 1
+
+        
 
 def part_one():
     total_points = 0
@@ -32,7 +48,14 @@ def part_one():
         opponent_pick = ord(opponent_pick) - ORD_A
         my_pick = ord(my_pick) - ORD_X
 
-        total_points += (my_pick + 1) + result_points(opponent_pick, my_pick)
+        # shapes are 0 indexed in normalization so add 1
+        
+        # part 1
+        #total_points += (my_pick + 1) + result_points(opponent_pick, my_pick)
+
+        # part 2 
+        desired_result = my_pick
+        total_points += desired_result * 3 + (compute_shape(opponent_pick, desired_result) + 1)
 
     print(total_points)
 
