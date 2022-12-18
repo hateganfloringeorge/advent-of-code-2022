642d6b88ecc5a4a9b415c03b997cf31ab937605e
hateganfloringeorge <hateganfloringeorge@gmail.com>
2022-12-18 16:05:15 +0200

    Ninth Day part twp

--- 9/ninth.py
+++ 9/ninth.py
@@ -56,6 +56,73 @@ def part_one():
 
     print(len(positions))
 
+def part_two():
+
+    N = 10
+    x_pos = [0] * N
+    y_pos = [0] * N
+    positions = {(0,0)}
+
+    for i in range(len(moves)):
+        direction, steps = moves[i].split(" ")
+        steps = int(steps)
+
+        for j in range(steps):
+            if direction == 'R':
+                x_pos[0] += 1
+
+            if direction == 'L':
+                x_pos[0] -= 1
+
+            if direction == 'D':
+                y_pos[0] -= 1
+
+            if direction == 'U':
+                y_pos[0] += 1
+
+            # iterate through all positions to see the ripple effect
+            k = 0
+            while k < N - 1 and (abs(x_pos[k] - x_pos[k+1]) > 1 
+                                    or abs(y_pos[k] - y_pos[k+1]) > 1):
+
+                offset_x = x_pos[k + 1] - x_pos[k]
+                offset_y = y_pos[k + 1] - y_pos[k]
+                                
+                if offset_x == 0 or offset_y == 0:
+                    # move horizontally
+                    if offset_x != 0:
+                        offset_x //= 2
+                    
+                    if offset_y != 0:
+                        offset_y //= 2
+
+                #add the case when moving diagonally twice
+                elif abs(offset_x) == 2 and abs(offset_y) == 2:
+                    offset_y //= 2
+                    offset_x //= 2
+
+                else:
+                    # move diagonally
+                    if abs(offset_x) == 1:
+                        offset_x = 0
+                        offset_y //= 2
+                    
+                    else:
+                        offset_y = 0
+                        offset_x //= 2
+                
+                # change position in list
+                x_pos[k + 1] = x_pos[k] + offset_x
+                y_pos[k + 1] = y_pos[k] + offset_y
+                
+                # add position to dictionary
+                if (k == N - 2):
+                    positions.add((x_pos[N-1], y_pos[N-1]))
+
+                k += 1
+                
+
+    print(len(positions))
 
 
 
@@ -66,4 +133,5 @@ with open('./9/input.txt') as f:
 
 moves = [s.strip() for s in moves]
 
-part_one()
+#part_one()
+part_two()
