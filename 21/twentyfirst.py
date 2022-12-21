0bccf45fdea7b34dd831cca88caf8ed983b16b6b
hateganfloringeorge <hateganfloringeorge@gmail.com>
2022-12-21 13:38:27 +0200

    Twentyfirst day part two

--- 21/twentyfirst.py
+++ 21/twentyfirst.py
@@ -1,7 +1,7 @@
 
 class Node:
     def __init__(self, name: str, isNumber: bool, number: int, operation: str, first_operand:str, second_operand:str):
-        self.str = name
+        self.name = name
         self.isNumber = isNumber
         self.number = number
         self.operation = operation
@@ -23,6 +23,14 @@ class Node:
             return value1 - value2
         else:
             return value1 / value2
+    
+    def check_humn(self):
+        if self.isNumber:
+            if self.name == "humn":
+                return True
+            return False
+        
+        return node_dict[self.first].check_humn() or node_dict[self.second].check_humn()
 
 
 
@@ -36,8 +44,51 @@ def part_one():
         else:
             node_dict[name] = Node(name, False, None, tokens[1], tokens[0], tokens[2])
 
-    root_value = node_dict['root'].get_value()
-    print(root_value)
+    # part one
+    # root_value = node_dict['root'].get_value()
+    # print(root_value)
+
+    # part two
+    node = node_dict['root']
+    result = 0
+
+    print(node_dict[node.second].check_humn())
+    #compute the result and then reverse operations just as in any ecuation
+    if node_dict[node.first].check_humn():
+        result = node_dict[node.second].get_value()
+        node = node_dict[node.first]
+    else:
+        result = node_dict[node.first].get_value()
+        node = node_dict[node.second]
+    
+    while node.name != 'humn':
+        other_value = 0
+        if node_dict[node.first].check_humn():
+            other_value = node_dict[node.second].get_value()
+            aux_node = node_dict[node.first]
+        else:
+            other_value = node_dict[node.first].get_value()
+            aux_node = node_dict[node.second]
+
+        if node.operation == '+':
+            result -= other_value
+        elif node.operation == '*':
+            result /= other_value
+        elif node.operation == '-':
+            if aux_node.name == node_dict[node.second].name:
+                result = other_value - result
+            else:
+                result += other_value
+        else:
+            if aux_node.name == node_dict[node.second].name:
+                result = other_value / result
+            else:
+                result *= other_value
+        
+        node = aux_node
+    
+    print(result)
+
 
 # main
 with open('./21/input.txt') as f:
